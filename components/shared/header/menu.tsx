51458242fd3635c1d7355c4a57c7b367d8699815
rcvsdigital <rcvs.digitalgroup@gmail.com>
2025-03-12 21:37:49 +0530

    page update

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,26 @@
+import {ShoppingCartIcon,UserIcon} from 'lucide-react'
+import Link from 'next/link'
+export default function Menu() {
+    return (
+        <div className='flex justify-end'>
+      <nav className='md:flex gap-3 hidden w-full'>
+     <Link href='/cart' className='header-button'>
+     <UserIcon className='w-6 h-6' />
+<span className='font-bold'>Sign in</span>
+     
+     </Link>
+     <Link href='/cart' className='header-button'>
+     <ShoppingCartIcon className='w-6 h-6' />
+     <span className='font-bold'>Cart</span>
+
+     </Link>
+      </nav>
+      
+          
+          
+            
+          
+      
+    </div>
+    )
+}
---
+++ components/shared/index.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Menu from './menu'
+
+import Search from './search'
+import data from '@/lib/data'
+import Sidebar from './sidebar'
+
+
+export default async function Header() {
+  
+  return (
+    <header className='bg-black  text-white'>
+      <div className='px-2'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center'>
+            <Link
+              href='/'
+              className='flex items-center header-button font-extrabold text-2xl m-1 '
+            >
+              <Image
+                src='/icons/logo.svg'
+                width={40}
+                height={40}
+                alt={`${APP_NAME} logo`}
+              />
+              {APP_NAME}
+            </Link>
+          </div>
+
+          <div className='hidden md:block flex-1 max-w-xl'>
+            <Search />
+          </div>
+          <Menu />
+        </div>
+        <div className='md:hidden block py-2'>
+          <Search />
+        </div>
+      </div>
+      <div className='flex items-center px-3 mb-[1px]  bg-gray-800'>
+        <Sidebar categories={categories} />
+        <div className='flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]'>
+          {data.headerMenus.map((menu) => (
+            <Link
+              href={menu.href}
+              key={menu.href}
+              className='header-button !p-2 '
+            >
+              {t('Header.' + menu.name)}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </header>
+  )
+}
